b0c47df5020c5b984342dc9abcb005f3054a8899
YeDongVibe <wkwhstladmss@naver.com>
2023-09-12 17:15:17 +0900

    json파일 구성 변경

--- Data/test10_1/Data_treate.py
+++ Data/test10_1/Data_treate.py
@@ -1,167 +1,111 @@
 import json
 import os
 
-# 파일 경로 및 이름 설정
-train_data_file = "C:/Ye_Dong/AI_Team_project_final/Data/test10/train_data.txt"
-test_data_file = "C:/Ye_Dong/AI_Team_project_final/Data/test10/test_data.txt"
-val_data_file = "C:/Ye_Dong/AI_Team_project_final/Data/test10/val_data.txt"
+# 데이터 파일 경로
+data_file = "./cleaned_trash.txt"
 
-data_file = "C:/Ye_Dong/AI_Team_project_final/Data/test10/cleaned_trash_renew.txt"
+# 이미지 디렉토리 경로
+image_dir = "C:/Ye_Dong/AI_Project_Img_Captioning-1/test10/test09finalimg"
 
-train_json_file = "train_renew.json"
-test_json_file = "test_renew.json"
-val_json_file = "val_renew.json"
+# Train 데이터 파일 경로
+train_data_file = "train_data.txt"
 
-# 이미지와 캡션을 저장할 리스트 초기화
-images = []
-annotations = []
+# Test 데이터 파일 경로
+test_data_file = "test_data.txt"
 
-# 이미지 파일 이름을 저장할 집합 초기화
-image_filenames = set()
+# val 데이터 파일 경로
+val_data_file = "val_data.txt"
 
-# 이미지 파일과 캡션 데이터를 읽어와서 처리
-with open(data_file, 'r') as data_file:
-    for line in data_file:
-        image_name, caption = line.strip().split('/')
-        
-        # 이미지 파일 이름이 이미 리스트에 있는 경우
-        if image_name in image_filenames:
-            continue
-        
-        # 이미지 정보 추가
+# Train 데이터를 저장할 JSON 파일 경로
+train_json_file = "train_renew_vs.json"
+
+# Test 데이터를 저장할 JSON 파일 경로
+test_json_file = "test_renew_vs.json"
+
+# val 데이터를 저장할 JSON 파일 경로
+val_json_file = "val_renew_vs.json"
+
+# 이미지 이름을 저장할 집합 (중복된 이름 방지)
+unique_image_names = set()
+
+# 함수 정의: 데이터를 JSON 형식으로 저장하는 함수
+def save_data_to_json(data, json_file):
+    with open(json_file, 'w') as json_output_file:
+        json.dump(data, json_output_file, indent=4)
+
+# 데이터 처리 함수
+def process_data(data_file, output_json_file, split_name):
+    data_images = []
+    data_annotations = []
+
+    with open(data_file, 'r') as data_file:
+        data = data_file.read().splitlines()
+
+    for i, line in enumerate(data):
+        # 이미지 파일 이름만 있는 경우, 캡션을 빈 문자열로 설정
+        image_name = line.strip()
+        caption = ""
+
+        # 이미지 파일 경로 설정
+        image_path = os.path.join(image_dir, image_name)
+        image_path = image_path.replace("\\", "/")
+
         image_info = {
-            "image_id": len(images) + 1,
-            "caption_id": len(annotations) + 1,
-            "caption": caption,
-            "height": 800,  # 이미지 높이 설정
-            "width": 600,   # 이미지 너비 설정
+            "image_id": i + 1,
+            "caption_id": i + 1,
+            "caption": caption.strip(),
+            "height": 800,  # 이미지의 높이 정보를 수정하세요
+            "width": 600,   # 이미지의 너비 정보를 수정하세요
             "file_name": image_name,
             "coco_url": f"https://example.com/images/{image_name}",
-            "image_path": f"C:/Ye_Dong/AI_Team_project_final/Data/test10/test10img/{image_name}"
+            "image_path": image_path
         }
-        images.append(image_info)
-        
-        # 캡션 정보 추가
+        data_images.append(image_info)
+
         annotation_info = {
-            "image_id": len(images),
-            "id": len(annotations) + 1,
-            "caption": caption
+            "image_id": i + 1,
+            "id": i + 1,
+            "caption": caption.strip()
         }
-        annotations.append(annotation_info)
-        
-        # 이미지 파일 이름 추가
-        image_filenames.add(image_name)
+        data_annotations.append(annotation_info)
 
-# 분류된 데이터를 JSON 파일로 저장
-def save_json(file_name, data):
-    with open(file_name, 'w') as json_file:
-        json.dump(data, json_file)
+    data_json = {
+        "info": info,
+        "licenses": licenses,
+        "images": data_images,
+        "annotations": data_annotations
+    }
 
-# train, test, val 데이터 분류 및 저장
-# train_data_file, test_data_file, val_data_file에서 이미지 파일 이름을 읽어와서 분류합니다.
-train_images = []
-test_images = []
-val_images = []
+    save_data_to_json(data_json, output_json_file)
+    print(f"{split_name} JSON file saved.")
 
-with open(train_data_file, 'r') as train_file:
-    train_image_names = train_file.read().splitlines()
 
-with open(test_data_file, 'r') as test_file:
-    test_image_names = test_file.read().splitlines()
-
-with open(val_data_file, 'r') as val_file:
-    val_image_names = val_file.read().splitlines()
-
-for image_info in images:
-    if image_info["file_name"] in train_image_names:
-        train_images.append(image_info)
-    elif image_info["file_name"] in test_image_names:
-        test_images.append(image_info)
-    elif image_info["file_name"] in val_image_names:
-        val_images.append(image_info)
-
-# 출력 - 각 데이터 세트의 이미지 개수
-print(f"Train 데이터 세트의 이미지 개수: {len(train_images)}")
-print(f"Test 데이터 세트의 이미지 개수: {len(test_images)}")
-print(f"Val 데이터 세트의 이미지 개수: {len(val_images)}")
-
-# 각각의 데이터를 JSON 파일로 저장
-train_data_dict = {
-    "info": {
-        "description": "YeDong 2023 Dataset",
-        "url": "http://cocodataset.org",
-        "version": "1.0",
-        "year": 2023,
-        "contributor": "LeeYeJin",
-        "date_created": "2023-09-09"
-    },
-    "licenses": [
-        {
-            "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
-            "id": 1,
-            "name": "Attribution-NonCommercial-ShareAlike License"
-        },
-        {
-            "url": "http://creativecommons.org/licenses/by-nc/2.0/",
-            "id": 2,
-            "name": "Attribution-NonCommercial License"
-        }
-    ],
-    "images": train_images,
-    "annotations": annotations
+# info 섹션 구성
+info = {
+    "description": "YeDong 2023 Dataset",
+    "url": "http://cocodataset.org",
+    "version": "1.0",
+    "year": 2023,
+    "contributor": "LeeYeJin",
+    "date_created": "2023-09-09"
 }
 
-test_data_dict = {
-    "info": {
-        "description": "YeDong 2023 Dataset",
-        "url": "http://cocodataset.org",
-        "version": "1.0",
-        "year": 2023,
-        "contributor": "LeeYeJin",
-        "date_created": "2023-09-09"
+# licenses 섹션 구성
+licenses = [
+    {
+        "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
+        "id": 1,
+        "name": "Attribution-NonCommercial-ShareAlike License"
     },
-    "licenses": [
-        {
-            "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
-            "id": 1,
-            "name": "Attribution-NonCommercial-ShareAlike License"
-        },
-        {
-            "url": "http://creativecommons.org/licenses/by-nc/2.0/",
-            "id": 2,
-            "name": "Attribution-NonCommercial License"
-        }
-    ],
-    "images": test_images,
-    "annotations": annotations
-}
+    {
+        "url": "http://creativecommons.org/licenses/by-nc/2.0/",
+        "id": 2,
+        "name": "Attribution-NonCommercial License"
+    }
+    # 필요한 만큼 라이선스 정보를 추가할 수 있습니다.
+]
 
-val_data_dict = {
-    "info": {
-        "description": "YeDong 2023 Dataset",
-        "url": "http://cocodataset.org",
-        "version": "1.0",
-        "year": 2023,
-        "contributor": "LeeYeJin",
-        "date_created": "2023-09-09"
-    },
-    "licenses": [
-        {
-            "url": "http://creativecommons.org/licenses/by-nc-sa/2.0/",
-            "id": 1,
-            "name": "Attribution-NonCommercial-ShareAlike License"
-        },
-        {
-            "url": "http://creativecommons.org/licenses/by-nc/2.0/",
-            "id": 2,
-            "name": "Attribution-NonCommercial License"
-        }
-    ],
-    "images": val_images,
-    "annotations": annotations
-}
-
-# JSON 파일로 저장
-save_json(train_json_file, train_data_dict)
-save_json(test_json_file, test_data_dict)
-save_json(val_json_file, val_data_dict)
+# 데이터 처리
+process_data(train_data_file, train_json_file, "Train")
+process_data(test_data_file, test_json_file, "Test")
+process_data(val_data_file, val_json_file, "Val")
