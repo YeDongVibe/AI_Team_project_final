d6b162b4825e4867b39df850f8b5a34111d877e9
Jieun Huh <entks301@naver.com>
2023-09-13 13:06:32 +0900

    'backProject'

--- Back/AI_Project_K3/src/main/java/edu/pnu/controller/FlaskConnectController.java
+++ Back/AI_Project_K3/src/main/java/edu/pnu/controller/FlaskConnectController.java
@@ -4,12 +4,15 @@ import java.util.Map;
 import java.util.Optional;
 
 import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import edu.pnu.domain.ImageEntity;
 import edu.pnu.persistence.ImageRepository;
@@ -20,42 +23,44 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/public/prediction")
 public class FlaskConnectController {
-	
+
 	private final FlaskCallService flaskService;
-	
+
 	private final ImageRepository imgRepo;
-	
+
 	@PostMapping("/put_image")
-	public ResponseEntity<String> processImage(@RequestBody Map<String, String> requestBody) {
-	    String imageUrl = requestBody.get("image_url");
+	public ResponseEntity<String> processImage(@RequestParam(name = "img", required = false) MultipartFile imgFile) {
+//	    String imageUrl = requestBody.get("image_url");
+		try {
+			// Flask 서버에 이미지 URL 전송
+			String flaskResponse = flaskService.sendImageToFlask(imgFile);
 
-	    // Flask 서버에 이미지 URL 전송
-	    String flaskResponse = flaskService.sendImageToFlask(imageUrl);
+			// Flask 서버에서 받은 응답을 JSON으로 파싱
+			// 예시: {"caption": "Generated caption for the image"}
+			JSONObject jsonResponse = new JSONObject(flaskResponse);
+			String caption = jsonResponse.getString("caption");
 
-	    // Flask 서버에서 받은 응답을 JSON으로 파싱
-	    // 예시: {"caption": "Generated caption for the image"}
-	    JSONObject jsonResponse = new JSONObject(flaskResponse);
-	    String caption = jsonResponse.getString("caption");
+			// 이미지 캡션을 저장
+			ImageEntity imgEntity = imgRepo.findByName(imgFile.getOriginalFilename()).orElse(new ImageEntity());
+			imgEntity.setCaption(caption);
+			imgRepo.save(imgEntity);
 
-	    // 이미지 캡션을 저장
-	    Optional<ImageEntity> imgOptional = imgRepo.findByName(imageUrl);
-	    if (imgOptional.isPresent()) {
-	        ImageEntity imgEntity = imgOptional.get();
-	        imgEntity.setCaption(caption);
-	        imgRepo.save(imgEntity);
-	    }
-
-	    return ResponseEntity.ok(caption);
+			return ResponseEntity.ok(caption);
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+					.body("Error processing image : " + e.getMessage());
+		}
 	}
-	
+
 	@GetMapping("/caption")
-	public ResponseEntity<String> receiveCaption(@RequestBody Map<String,String> json){
+	public ResponseEntity<String> receiveCaption(@RequestBody Map<String, String> json) {
 		String caption = json.get("caption");
 		ImageEntity imgEntity = new ImageEntity();
 		imgEntity.setCaption(caption);
 		imgRepo.save(imgEntity);
-		
+
 		return ResponseEntity.ok("upload Success.");
-		
+
 	}
 }
--- Back/AI_Project_K3/src/main/java/edu/pnu/service/FlaskCallService.java
+++ Back/AI_Project_K3/src/main/java/edu/pnu/service/FlaskCallService.java
@@ -1,34 +1,57 @@
 package edu.pnu.service;
 
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 //@RequiredArgsConstructor
 public class FlaskCallService {
 	
-	public String sendImageToFlask(String imgURL) {
+	public String sendImageToFlask(MultipartFile imgFile) {
 		
+		// flask url
 		String flaskURL = "http://127.0.0.1:5000/process_image";
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
 		
+		// get img name
+		String imgName = imgFile.getOriginalFilename();
+		
+		// local file directory
+		String localImg = "C://AI_Result_img/" + imgName;
+		
+		// generate http post
+		RestTemplate restTemplate = new RestTemplate();
+		
+		// Multivalue Map
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		
+		// local file -> file resource
+		File localFile = new File(localImg);
+		
+		map.add("img", new FileSystemResource(localFile));
+		
+		// setting HTTP headers
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.MULTIPART_FORM_DATA);
 		
 		// flask 서버로 전송할 데이터 설정
-		Map<String, String> requestBody = new HashMap<>();
-		requestBody.put("image_url", imgURL);
+//		Map<String, String> requestBody = new HashMap<>();
+//		requestBody.put("image_url", imgURL);
 		
 		// 왜 여기서 자꾸 예외처리 나는지 모르겠네 왜진
-		HttpEntity<Map<String,String>> requestEntity = new HttpEntity<>(requestBody, headers);
+		HttpEntity<MultiValueMap<String,Object>> requestEntity = new HttpEntity<>(map, headers);
 		ResponseEntity<String> response = restTemplate.exchange(
 				flaskURL,
 				HttpMethod.POST,
