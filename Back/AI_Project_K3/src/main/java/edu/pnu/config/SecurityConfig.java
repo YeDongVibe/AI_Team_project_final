07837715f2cb17e8598c7611ea53962ed8e04df2
Jieun Huh <entks301@naver.com>
2023-09-18 10:55:20 +0900

    config

--- Back/AI_Project_K3/src/main/java/edu/pnu/config/SecurityConfig.java
+++ Back/AI_Project_K3/src/main/java/edu/pnu/config/SecurityConfig.java
@@ -42,7 +42,7 @@ public class SecurityConfig {
 						// .requestMatchers("/admin/**")
 						// .hasRole("ADMIN")
 						// 권한 설정 추가 기억이겅 !!
-						.requestMatchers("/public/**", "/manager/**", "/images/**", "/testimg").permitAll();
+						.requestMatchers("/public/**", "/manager/**", "/images/**", "/testimg", "/find/**").permitAll();
 				// .anyRequest().permitAll();
 				// .authenticated()
 				// .and().exceptionHandling().authenticationEntryPoint(new
